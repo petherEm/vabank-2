d994641b3eec5817e8ce41a348aa6cb959c084ee
petherEm <pether.maciejewski@gmail.com>
2025-08-25 16:14:27 +0200

    eslint update 1.0

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,22 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "@typescript-eslint": typescriptPlugin
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "react-hooks/rules-of-hooks": "off",
+      "no-unused-vars": "off",
+      "no-unused-expressions": "off",
+      "non-nullish-assertion": "off",
+    }
+  }
 ];
 
 export default eslintConfig;
